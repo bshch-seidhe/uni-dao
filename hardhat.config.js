f9ea4e199f379cb56bf6524d5c52082ee353f6a5
Bohdan Shcherbak <b.shcherbak@gmail.com>
2026-07-23 00:09:15 +0300

    updated config file for the HardHat tool

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,25 @@ const { SEPOLIA_RPC_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+      viaIR: true,
+      evmVersion: "cancun",
+    },
+  },
   networks: {
     sepolia: {
-      url: SEPOLIA_RPC_URL || "",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
